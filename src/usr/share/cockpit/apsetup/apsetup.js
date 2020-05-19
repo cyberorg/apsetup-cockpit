69f691b807c2b3a3c22e89092d626b65797c5c4b
Jigish Gohil <cyberorg@cyberorg.info>
2020-05-19 10:31:32 +0530

    prettier js

--- src/usr/share/cockpit/apsetup/apsetup.js
+++ src/usr/share/cockpit/apsetup/apsetup.js
@@ -9,31 +9,42 @@ const result = document.getElementById("result");
 const button = document.getElementById("apapply");
 
 function apsetup_run() {
-    cockpit.spawn(["/usr/share/cockpit/apsetup/apsetup-cockpit", NAP_SSID.value, NAP_PASS.value, NAP_IP.value, NNET_CONNECT.value] , { superuser: true })
-        .stream(apapply_output)
-        .then(apapply_success)
-        .catch(apapply_fail);
+  cockpit
+    .spawn(
+      [
+        "/usr/share/cockpit/apsetup/apsetup-cockpit",
+        NAP_SSID.value,
+        NAP_PASS.value,
+        NAP_IP.value,
+        NNET_CONNECT.value,
+      ],
+      { superuser: true }
+    )
+    .stream(apapply_output)
+    .then(apapply_success)
+    .catch(apapply_fail);
 
-    result.innerHTML = "";
-    output.innerHTML = "";
+  result.innerHTML = "";
+  output.innerHTML = "";
 }
 
 function apapply_success() {
-    result.style.color = "green";
-    result.innerHTML = "success";
+  result.style.color = "green";
+  result.innerHTML = "success";
 }
 
 function apapply_fail() {
-    result.style.color = "red";
-    result.innerHTML = "fail";
+  result.style.color = "red";
+  result.innerHTML = "fail";
 }
 
 function apapply_output(data) {
-    output.append(document.createTextNode(data));
+  output.append(document.createTextNode(data));
 }
 
 // Connect the button to starting the "ping" process
 button.addEventListener("click", apsetup_run);
 
 // Send a 'init' message.  This tells integration tests that we are ready to go
-cockpit.transport.wait(function() { });
+cockpit.transport.wait(function () {});
+
