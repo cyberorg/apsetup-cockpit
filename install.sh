e701f1db9493e242264479c9969491abc450b10b
Jigish Gohil <cyberorg@cyberorg.info>
2020-05-24 18:26:09 +0530

    add install.sh script

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Colors to use for output
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+
+
+if [[ $EUID -ne 0 ]]; then
+   echo -e "${RED}This script must be run as root"
+   exit
+fi
+mkdir -p /usr/share/cockpit/apsetup
+cd /usr/share/cockpit/apsetup/
+for i in networkvariables.sh ap.js apsetup.js index.html manifest.json apdnsmasqsetup 90apnmdispatcher network.css.gz; do
+	wget -c https://raw.githubusercontent.com/cyberorg/apsetup-cockpit/ako/src/usr/share/cockpit/apsetup/$i
+done
+echo -e "${GREEN} Cockpit APsetup plugin installed"
