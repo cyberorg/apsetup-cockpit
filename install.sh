d259b81fdc9f85a41ade1c75d156923bd1cd1949
Jigish Gohil <cyberorg@cyberorg.info>
2020-05-29 08:36:30 +0530

    make bash scripts executable

--- install.sh
+++ install.sh
@@ -14,4 +14,5 @@ cd /usr/share/cockpit/apsetup/
 for i in networkvariables.sh apsetup.js index.html manifest.json apdnsmasqsetup 90apnmdispatcher network.css.gz; do
 	wget -c https://raw.githubusercontent.com/cyberorg/apsetup-cockpit/ako/src/usr/share/cockpit/apsetup/$i
 done
+chmod +x /usr/share/cockpit/apsetup/apdnsmasqsetup /usr/share/cockpit/apsetup/90apnmdispatcher
 echo -e "${GREEN} Cockpit APsetup plugin installed"
