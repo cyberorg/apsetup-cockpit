c1906551d27542a62616f05bf6c30fd739985768
Jigish Gohil <cyberorg@cyberorg.info>
2020-06-04 16:08:40 +0530

    overwrite files so update is possible

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ fi
 test -f /etc/NetworkManager/dispatcher.d/90apnmdispatcher && rm /etc/NetworkManager/dispatcher.d/90apnmdispatcher
 mkdir -p /usr/share/cockpit/apsetup 
 for i in networkvariables.sh apsetup.js index.html manifest.json apdnsmasqsetup 90apnmdispatcher network.css.gz; do
-	wget -c https://raw.githubusercontent.com/cyberorg/apsetup-cockpit/master/src/usr/share/cockpit/apsetup/$i -O /usr/share/cockpit/apsetup/$i
+	wget https://raw.githubusercontent.com/cyberorg/apsetup-cockpit/master/src/usr/share/cockpit/apsetup/$i -O /usr/share/cockpit/apsetup/$i
 done
 chmod +x /usr/share/cockpit/apsetup/apdnsmasqsetup /usr/share/cockpit/apsetup/90apnmdispatcher
 echo -e "Cockpit APsetup plugin installed"
