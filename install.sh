7ccd05b384e442ae5acbe255c4f70cddbc244520
Jigish Gohil <cyberorg@cyberorg.info>
2020-06-04 16:00:41 +0530

    remove old file

--- install.sh
+++ install.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 
-# Colors to use for output
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-
-
 if [[ $EUID -ne 0 ]]; then
-   echo -e "${RED}This script must be run as root"
+   echo -e "This script must be run as root"
    exit
 fi
 mkdir -p /usr/share/cockpit/apsetup
+test -f /etc/NetworkManager/dispatcher.d/90apnmdispatcher && rm /etc/NetworkManager/dispatcher.d/90apnmdispatcher
 for i in networkvariables.sh apsetup.js index.html manifest.json apdnsmasqsetup 90apnmdispatcher network.css.gz; do
 	wget -c https://raw.githubusercontent.com/cyberorg/apsetup-cockpit/ako/src/usr/share/cockpit/apsetup/$i -O /usr/share/cockpit/apsetup/$i
 done
 chmod +x /usr/share/cockpit/apsetup/apdnsmasqsetup /usr/share/cockpit/apsetup/90apnmdispatcher
-echo -e "${GREEN} Cockpit APsetup plugin installed"
+echo -e "Cockpit APsetup plugin installed"
