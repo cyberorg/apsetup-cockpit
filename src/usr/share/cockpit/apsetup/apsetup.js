83be1ff495d88b7b32ba94644a241ef4e521d428
Jigish Gohil <cyberorg@cyberorg.info>
2021-03-06 09:57:02 +0530

    add mode

--- src/usr/share/cockpit/apsetup/apsetup.js
+++ src/usr/share/cockpit/apsetup/apsetup.js
@@ -2,7 +2,7 @@ cockpit
     .spawn(["bash", "-c", "if ! test -f /etc/ap.conf; then touch /etc/ap.conf && chmod 600 /etc/ap.conf; fi"], {
       superuser: true,
     })
-var CVAR = [ "AP_SSID", "AP_PASS", "AP_IP", "NET_CONNECT", "AP_IFACE", "output", "result", "apapply" ];
+var CVAR = [ "AP_SSID", "AP_PASS", "AP_IP", "NET_CONNECT", "AP_IFACE", "AP_MODE", "output", "result", "apapply" ];
 VLen = CVAR.length;
 var x = "",
   i;
@@ -56,7 +56,12 @@ function apsetup_run() {
       "AP_IFACE" +
       "=" +
       AP_IFACE.value +
-      "\n";
+      "\n" +
+      "AP_MODE" +
+      "=" +
+      AP_MODE.value +
+      "\n"
+		  ;
     cockpit.file("/etc/ap.conf", { superuser: "try" }).replace(configfile);
     function spawncmd() {
       if (idout == 0) {
@@ -65,7 +70,7 @@ function apsetup_run() {
             [
               "bash",
               "-c",
-              "echo Please wait...;. /etc/ap.conf; nmcli con modify ap ssid $AP_SSID ipv4.addresses $AP_IP/24 wifi-sec.psk $AP_PASS",
+              "echo Please wait...;. /etc/ap.conf; nmcli con modify ap ssid $AP_SSID ipv4.addresses $AP_IP/24 wifi-sec.psk $AP_PASS mode $AP_MODE",
             ],
             { superuser: true }
           )
@@ -76,7 +81,7 @@ function apsetup_run() {
             [
               "bash",
               "-c",
-              "echo Please wait...;. /etc/ap.conf; nmcli con add type wifi ifname $AP_IFACE con-name ap autoconnect yes ssid $AP_SSID ipv4.method manual ipv4.addresses $AP_IP/24 wifi-sec.key-mgmt wpa-psk wifi-sec.psk $AP_PASS 802-11-wireless.mode ap",
+              "echo Please wait...;. /etc/ap.conf; nmcli con add type wifi ifname $AP_IFACE con-name ap autoconnect yes ssid $AP_SSID ipv4.method manual ipv4.addresses $AP_IP/24 wifi-sec.key-mgmt wpa-psk wifi-sec.psk $AP_PASS 802-11-wireless.mode $AP_MODE",
             ],
             { superuser: true }
           )
@@ -228,6 +233,11 @@ cockpit
         document.getElementById("AP_IFACE").value = result.AP_IFACE;
     } else {
       get_wl();
+    }
+    if (result.AP_MODE) {
+        document.getElementById("AP_MODE").value = result.AP_MODE;
+    } else {
+        document.getElementById("AP_MODE").value = "ap";
     }
   });
 // Send a 'init' message.  This tells integration tests that we are ready to go
